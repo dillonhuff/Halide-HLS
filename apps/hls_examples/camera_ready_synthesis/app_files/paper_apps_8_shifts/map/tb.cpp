cbd6950df7d055254ad827ed647eaff9601c4ceb
Dillon Huff <dillonhuff@gmail.com>
2020-03-13 22:56:06 -0700

    Now have tb running for map

--- apps/hls_examples/camera_ready_synthesis/app_files/paper_apps_8_shifts/map/tb.cpp
+++ apps/hls_examples/camera_ready_synthesis/app_files/paper_apps_8_shifts/map/tb.cpp
@@ -8,6 +8,9 @@
 using namespace std;
 using namespace nlohmann;
 
+typedef Stencil<uint8_t, 1, 1> stencil;
+typedef AxiPackedStencil<uint8_t, 1, 1> axi_stencil;
+
 template<typename T>
 vector<T>
 load_vals(const std::string& file) {
@@ -18,11 +21,11 @@ load_vals(const std::string& file) {
   j << infile;
   cout << "read in file" << endl;
 
+  vector<T> vals;
   for (auto e : j) {
-    cout << "  " << e << endl;
+    vals.push_back(e);
   }
 
-  vector<T> vals;
   return vals;
 }
 
@@ -30,6 +33,26 @@ int main() {
 
   vector<uint8_t> vals =
     load_vals<uint8_t>("../../../test_io_seq_form/paper_apps_8_shifts/map_input.json");
+
+  hls::stream<axi_stencil> ins, outs;
+  for (auto v : vals) {
+    stencil a;
+    a(0, 0) = v;
+    axi_stencil as;
+    as = a;
+    ins.write(as);
+  }
+
+  hls_target(ins, outs);
+
+  vector<uint8_t> correct_outputs =
+    load_vals<uint8_t>("../../../test_io_seq_form/paper_apps_8_shifts/map_output.json");
+  for (auto expected : correct_outputs) {
+    uint8_t actual = outs.read().value;
+    cout << "actual   : " << (int) actual << endl;
+    cout << "expected : " << (int) expected << endl;
+    assert(expected == actual);
+  }
 
   return 0;
 }
