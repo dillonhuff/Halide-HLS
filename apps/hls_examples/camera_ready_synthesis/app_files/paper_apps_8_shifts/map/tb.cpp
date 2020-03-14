1203af72be7d5397306bf28bbd60ecc262118ae5
Dillon Huff <dillonhuff@gmail.com>
2020-03-13 22:45:21 -0700

    Adding tb loads

--- apps/hls_examples/camera_ready_synthesis/app_files/paper_apps_8_shifts/map/tb.cpp
+++ apps/hls_examples/camera_ready_synthesis/app_files/paper_apps_8_shifts/map/tb.cpp
@@ -1,8 +1,35 @@
+#include <iostream>
 #include <vector>
+#include <fstream>
+
+#include "json.h"
 #include "hls_target.h"
 
+using namespace std;
+using namespace nlohmann;
+
+template<typename T>
+vector<T>
+load_vals(const std::string& file) {
+  cout << "Opening file: " << file << endl;
+
+  ifstream infile(file);
+  json j;
+  j << infile;
+  cout << "read in file" << endl;
+
+  for (auto e : j) {
+    cout << "  " << e << endl;
+  }
+
+  vector<T> vals;
+  return vals;
+}
 
 int main() {
+
+  vector<uint8_t> vals =
+    load_vals<uint8_t>("../../../test_io_seq_form/paper_apps_8_shifts/map_input.json");
 
   return 0;
 }
