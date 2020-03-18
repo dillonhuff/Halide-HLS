5073f0721adc95ba6c1310cf55e673ac0badaf79
Dillon Huff <dillonhuff@gmail.com>
2020-03-17 19:44:07 -0700

    Added sharpen big shift

---
+++ apps/hls_examples/camera_ready_synthesis/app_files/big_apps_8_shifts/sharpen/pipeline.cpp
@@ -0,0 +1,112 @@
+#include "Halide.h"
+#include <stdio.h>
+
+using namespace Halide;
+
+Var x("x"), y("y"), c("c");
+Var xo("xo"), yo("yo"), xi("xi"), yi("yi");
+
+class MyPipeline {
+public:
+    ImageParam input;
+    Func output, hw_input, hw_output, mul, kernel, kernel1, mul1, diff, avg, h;
+    Func delayed_input, delayed_kernel;
+
+    RDom win, win1;
+    std::vector<Argument> args;
+
+    MyPipeline()
+        : input(UInt(8), 2),
+        delayed_input("delayed_input"),
+          output("output"),
+          hw_input("hw_input"),
+          hw_output("hw_output"),
+          mul("mul"),
+          diff("diff"),
+          win(0, 3, 0, 3),
+          win1(0, 2, 0, 2)
+    {
+      delayed_kernel(x, y) = 0;
+      delayed_kernel(2, 2) = 1;
+      delayed_kernel.bound(x, 0, 3);
+      delayed_kernel.bound(y, 0, 3);
+
+      kernel(x, y) = 0;
+      kernel(0, 0) = 0;
+      kernel(1, 0) = 1;
+      kernel(2, 0) = 0;
+
+      kernel(0, 1) = 1;
+      kernel(1, 1) = 2;
+      kernel(2, 1) = 1;
+
+      kernel(0, 2) = 0;
+      kernel(1, 2) = 1;
+      kernel(2, 2) = 0;
+      kernel.bound(x, 0, 3);
+      kernel.bound(y, 0, 3);
+
+      hw_input(x, y) = cast<uint8_t>(input(x, y));
+
+      delayed_input(x, y) = 0;
+      delayed_input(x, y) += cast<uint8_t>(hw_input(win.x + x, win.y + y)*delayed_kernel(win.x, win.y));
+      
+      mul(x, y) = 0;
+      mul(x, y) += cast<uint8_t>(cast<uint8_t>(hw_input(win.x + x, win.y + y)) << kernel(win.x, win.y));
+
+      avg(x, y) = cast<uint8_t>(cast<uint8_t>(mul(x, y)) >> 4);
+
+      h(x, y) =
+        select(((cast<uint8_t>(delayed_input(x, y)) - cast<uint8_t>(avg(x, y))) > 15) ||
+            (cast<uint8_t>(avg(x, y)) - cast<uint8_t>(delayed_input(x, y)) > 15),
+            cast<uint8_t>(delayed_input(x, y)) - cast<uint8_t>(avg(x, y)),
+            0);
+
+      diff(x, y) = cast<uint8_t>(delayed_input(x, y))
+        + (cast<uint8_t>(h(x, y)) >> 2);
+      
+      hw_output(x, y) = cast<uint8_t>(diff(x, y));
+      output(x, y) = hw_output(x, y);
+
+      args = {input};
+    }
+
+    void compile_hls() {
+        std::cout << "\ncompiling HLS code..." << std::endl;
+
+        hw_input.compute_root();
+        hw_output.compute_root();
+
+        hw_output.tile(x, y, xo, yo, xi, yi, 1920 - 2, 1080 - 2)
+          .reorder(xi, yi, xo, yo);
+        hw_output.accelerate({hw_input}, xi, xo);
+
+        kernel.compute_at(hw_output, xo).unroll(x).unroll(y);
+        delayed_kernel.compute_at(hw_output, xo).unroll(x).unroll(y);
+        
+        mul.linebuffer();
+        mul.update(0)
+          .unroll(win.x).unroll(win.y);
+
+        delayed_input.linebuffer();
+        delayed_input.update(0)
+          .unroll(win.x).unroll(win.y);
+
+        output.print_loop_nest();
+
+        // Create the target for HLS simulation
+        Target hls_target = get_target_from_environment();
+        hls_target.set_feature(Target::CPlusPlusMangling);
+        output.compile_to_lowered_stmt("pipeline_hls.ir.html", args, HTML, hls_target);
+        output.compile_to_hls("pipeline_hls.cpp", args, "pipeline_hls", hls_target);
+        output.compile_to_header("pipeline_hls.h", args, "pipeline_hls", hls_target);
+
+    }
+};
+
+int main(int argc, char **argv) {
+    MyPipeline p2;
+    p2.compile_hls();
+
+    return 0;
+}
---
+++ apps/hls_examples/camera_ready_synthesis/app_files/big_apps_8_shifts/sharpen/tb.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+#include "json.h"
+#include "hls_target.h"
+
+using namespace std;
+using namespace nlohmann;
+
+typedef Stencil<uint8_t, 1, 1> stencil;
+typedef AxiPackedStencil<uint8_t, 1, 1> axi_stencil;
+
+template<typename T>
+vector<T>
+load_vals(const std::string& file) {
+  cout << "Opening file: " << file << endl;
+
+  ifstream infile(file);
+  json j;
+  j << infile;
+  cout << "read in file" << endl;
+
+  vector<T> vals;
+  for (auto e : j) {
+    vals.push_back(e);
+  }
+
+  return vals;
+}
+
+int main(const int argc, const char** argv) {
+
+  assert(argc == 3);
+
+  string in_file = argv[1];
+  string out_file = argv[2];
+
+  int in_rows = 1080;
+  int in_cols = 1920;
+  int in_pixels = in_rows*in_cols;
+
+  int out_rows = in_rows - 2;
+  int out_cols = in_cols - 2;
+
+
+  int out_delay = in_cols*2 + 2;
+
+  vector<uint8_t> vals =
+    load_vals<uint8_t>(in_file);
+  assert(in_pixels == vals.size());
+
+  hls::stream<axi_stencil> ins, outs;
+  for (auto v : vals) {
+    stencil a;
+    a(0, 0) = v;
+    axi_stencil as;
+    as = a;
+    ins.write(as);
+  }
+
+
+  hls_target(ins, outs);
+
+  //assert(false);
+
+  vector<uint8_t> correct_outputs =
+    load_vals<uint8_t>(out_file);
+
+  for (int r = 0; r < out_rows; r++) {
+    for (int c = 0; c < out_cols; c++) {
+      uint8_t actual = outs.read().value;
+
+      int pos = out_delay + r*in_cols + c;
+      uint8_t expected = correct_outputs.at(pos);
+
+      cout << "pos = " << pos << endl;
+      cout << "actual(" << c << ", " << c << ")   : " << (int) actual << endl;
+      cout << "expected(" << c << ", " << c << ") : " << (int) expected << endl;
+
+      if (expected != 253) {
+        assert(expected == actual);
+      }
+    }
+  }
+
+  return 0;
+}
